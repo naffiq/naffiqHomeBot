a6c97d850bd36b3f875b734481706a543f6d43ca
naffiq <abdu.galymzhan@gmail.com>
2017-01-26 02:58:33 +0600

    trying to fullscreen youtube vid after play

--- index.js
+++ index.js
@@ -35,15 +35,15 @@ bot.onText(/\/volume (.+)/, (msg, match) => {
 bot.onText(/http:\/\/www\.youtube\.com\/watch\?v=(.+)/, (msg, match) => {
 	var chatId = msg.chat.id;
 
-	if (typeof match[1] !== undefined) {
-		let url = match[1];
+	if (typeof match[0] !== undefined) {
+		let url = match[0];
 		console.log(url);
 		bot.sendMessage(chatId, `Starting video playback`);	
 		exec(`chromium-browser --app='http://www.youtube.com/watch_popup?v=${url}'`, (error, stdout, stderr) => {
 			bot.sendMessage(chatId, `Video playback is over`);
 		});
-		sleep(9);
-		exec(`xdotool key F11`, (error, stdout, stderr) => {});	
+		sleep(15);
+		exec(`xdotool key F`, (error, stdout, stderr) => {});	
 	}
 });
 

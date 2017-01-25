ed1e2b9c38d8359b0199a1a7641da8c9c076865e
naffiq <abdu.galymzhan@gmail.com>
2017-01-26 02:14:00 +0600

    Opening youtube videos

--- index.js
+++ index.js
@@ -31,3 +31,25 @@ bot.onText(/\/volume (.+)/, (msg, match) => {
 		});
 	}
 });
+
+bot.onText(/\/volume (.+)/, (msg, match) => {
+	var chatId = msg.chat.id;
+
+	if (typeof match[1] !== undefined) {
+		let level = match[1];
+		exec(`amixer set PCM ${level}%`, (error, stdout, stderr) => {
+			bot.sendMessage(chatId, `Volume set to ${level}%`);
+		});
+	}
+});
+
+bot.onText(/http:\/\/www.youtube.com\/watch?v=(.+)/, (msg, match) => {
+	var chatId = msg.chat.id;
+
+	if (typeof match[0] !== undefined) {
+		let url = match[0];
+		exec(`chromium-browser --app='${url}'`, (error, stdout, stderr) => {
+			bot.sendMessage(chatId, `Video is now live on your screen`);
+		});
+	}
+});

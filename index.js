936b37f67caf53364fef33c198520c47f4b9bad5
naffiq <abdu.galymzhan@gmail.com>
2017-01-26 01:56:52 +0600

    Changed bot library

--- index.js
+++ index.js
@@ -1,60 +1,19 @@
-var telegram = require('telegram-bot-api');
+var telegram = require('node-telegram-bot-api');
 var { botToken } = require('./config.js');
 
 var { exec } = require('child_process');
 
-var api = new telegram({
-	token: botToken,
-	updates: {
-		enabled: true
-	}
-});
+var bot = new telegram(botToken, { polling: true });
 
-api.on('message', (message) => {
-
-	// Taking a photo of my home
-	if (message.text === '/photo') {
-		console.log(message);
-
-		api.sendMessage({
-			'chat_id': message.chat.id,
-			'text': 'Taking a photo...'
-		}).then(() => {
-			exec('fswebcam -p YUYV photo.jpg', (error, stdout, stderr) => {
+bot.onText(/\/photo/, (msg, match) => {
+	var chatId = msg.chat.id;
+	exec('fswebcam -p YUYV photo.jpg', (error, stdout, stderr) => {
 			
-				if (!error) {
-					api.sendPhoto({
-						chat_id: message.chat.id,
-						caption: 'Photo of my home',
-						photo: './photo.jpg'
-					});
-				} else {
-					api.sendMessage({
-						'chat_id': message.chat.id,
-						'text': 'There was an error making a photo'
-					})
-				}
+		if (!error) {
+			bot.sendPhoto(chatId, './photo.jpg', {caption: 'Photo of my home'});
+		} else {
+			bot.sendMessage(chatId, 'There was an error making a photo');
+		}
 
-			});
-		});
-	}
-
-	// Setting a volume
-	if (message.text.includes('/volume')) {
-		let level = parseInt(message.text.remove('/volume'));
-		console.log(level);
-		// exec('fswebcam -p YUYV photo.jpg', (error, stdout, stderr) => {
-			
-		// 	api.sendMessage({
-		// 		'chat_id': message.chat.id,
-		// 		'text': 'There was an error making a photo'
-		// 	});
-
-		// });
-	}
-
-	// Play youtube video
-	if (message.text.includes('youtube')) {
-		console.log(message);
-	}
+	});
 });
